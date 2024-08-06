82be33f15888329edb2315f428f2108a7ae38e36
Muhammad Ahmad <123493085+ahmadmac01@users.noreply.github.com>
2024-08-06 12:44:38 +0500

    Add files via upload

---
+++ app (6).py
@@ -0,0 +1,19 @@
+import gradio as gr
+import torch
+from diffusers import DiffusionPipeline
+
+pipeline = DiffusionPipeline.from_pretrained("ahmadmac/stray-cat")
+
+def generate_image(prompt, output_file="generated_image.png"):
+  with torch.autocast("cpu"):  
+    image = pipeline(prompt=prompt).images[0]
+  image.save(output_file)
+  return output_file
+
+demo = gr.Interface(
+    fn=generate_image,
+    inputs=gr.Textbox(label="Prompt"),
+    outputs=gr.Image(label="Generated Image")
+)
+
+demo.launch()
